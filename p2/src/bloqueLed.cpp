2aba719cf1453733f19d9676edcb1fa39566aadd
ccamposfuentes <carlos@ccamposfuentes.es>
2016-03-16 20:06:36 +0100

    p2

---
+++ p2/src/bloqueLed.cpp
@@ -0,0 +1,13 @@
+/**
+@file
+@author Carlos Campos
+@date 16 de marzo de 2015
+@brief Funciones de manejo de bloqueLeds
+*/
+
+/**
+@brief enciende el LED @p pos del @c bloqueLed @p b
+@param b 	el @c bloqueLed cuyo LED se quiere activar
+@param pos 	el LED dentro de @p b que se quiere activar (0 m�s a la derecha)
+*/
+void on(bloqueLed &b, int pos);
