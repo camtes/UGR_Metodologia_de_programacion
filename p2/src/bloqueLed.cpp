f866db2518d7d0af0b65cc69cb0bb24b09e03b44
ccamposfuentes <carlos@ccamposfuentes.es>
2016-03-16 20:51:52 +0100

    bloqueled

--- p2/src/bloqueLed.cpp
+++ p2/src/bloqueLed.cpp
@@ -7,7 +7,7 @@
 
 /**
 @brief enciende el LED @p pos del @c bloqueLed @p b
-@param b 	el @c bloqueLed cuyo LED se quiere activar
+@param b el @c bloqueLed cuyo LED se quiere activar
 @param pos 	el LED dentro de @p b que se quiere activar (0 m�s a la derecha)
 */
 void on(bloqueLed &b, int pos);
