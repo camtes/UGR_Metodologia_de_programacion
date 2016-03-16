3c6fcbe184cd7b3773bcd7867f864aa2b059804b
ccamposfuentes <carlos@ccamposfuentes.es>
2016-03-16 21:04:25 +0100

    Reparto de tareas

--- p2/src/bloqueLed.cpp
+++ p2/src/bloqueLed.cpp
@@ -1,8 +1,5 @@
 /**
-@file
-@author Carlos Campos, Juan David Vargas Jimenez
-@date 16 de marzo de 2015
-@brief Funciones de manejo de bloqueLeds
+Creado por Carlos Campos, Juan David Vargas Jimenez
 */
 
 #include "bloqueLed.h"
@@ -10,43 +7,51 @@
 
 using namespace std;
 
+// Juanda
 void on(bloqueLed &b, int pos){
 
 }
 
-
+// Carlos
 void off(bloqueLed &b, int pos){
-
+  unsigned char MASK = 1;
+  MASK << pos-1;
 }
 
-
+// Juanda
 bool get(bloqueLed b, int pos){
 
 }
 
-
+// Carlos
 void print(bloqueLed b){
 
 }
 
-
+// Juanda
 void encender(bloqueLed &b){
 
 }
 
-
+// Carlos
 void apagar(bloqueLed &b){
 
 }
 
+// Juanda
 void asignar(bloqueLed &b, const bool v[]){
 
 }
 
+// Carlos
 void volcar(bloqueLed b, bool v[]){
 
 }
 
+// Juanda
 void encendidos(bloqueLed b, int posic[], int &cuantos){
 
 }
+
+// Carlos
+// el bloqueLedToString pa' mi
