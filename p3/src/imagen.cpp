0c53740674d741e6fb442ec77dea54ef36a6615c
ccamposfuentes <carlos@ccamposfuentes.es>
2016-03-30 21:09:10 +0200

    Subo mis funciones

--- p3/src/imagen.cpp
+++ p3/src/imagen.cpp
@@ -58,23 +58,13 @@ byte getPos(int i){
 
 // Carlos
 bool leerImagen(const char nombreFichero[]) {
-  /**
-  @brief Carga una imagen desde un fichero
-  @param nombreFichero nombre del fichero que contiene la imagen
-  @retval true 	si ha tenido éxito en la lectura
-  @retval false 	si se ha producido algún error en la lectura
-
-  Lee desde disco los datos de la imagen llamada @a nombreFichero y la
-  guarda en la memoria. La función debe asegurarse de que la imagen es de un
-  tipo de imagen conocido y de que su tamaño es menor del tamaño máximo permitido (@c MAXDATOS).
-  */
-
   bool result = false;
 
-
   TipoImagen tipo = infoPGM(nombreFichero[], nfilas, ncolumnas)
-  if ( tipo != IMG_DESCONOCIDO) {
-
+  if (tipo != IMG_DESCONOCIDO) {
+    if (ncolumnas*nfilas < MAXDATOS) {
+      result = true;
+    }
   }
 
   return result;
@@ -82,5 +72,5 @@ bool leerImagen(const char nombreFichero[]) {
 
 // Juanda
 bool escribirImagen(const char nombreFichero[], bool esBinario){
-
+  
 }
