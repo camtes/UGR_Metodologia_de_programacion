c7b08191aa5b167db1a8e58bee44085fb70c93dc
SI2 Soluciones <si2@si2.info>
2016-03-30 20:17:27 +0200

    asignacion de tareas

--- p3/src/imagen.cpp
+++ p3/src/imagen.cpp
@@ -4,22 +4,32 @@
 
 using namespace std;
 
+// Carlos
 Imagen(int filas, int columnas);
 
+// Juanda
 void crear(int filas, int columnas);
 
+// Carlos
 int filas();
 
+// Juanda
 int columnas();
 
+// Carlos
 void set(int y, int x, byte v);
 
+// Juanda
 byte get(int y, int x);
 
+// Carlos
 void setPos(int i, byte v);
 
+// Juanda
 byte getPos(int i);
 
+// Carlos
 bool leerImagen(const char nombreFichero[]);
 
+// Juanda
 bool escribirImagen(const char nombreFichero[], bool esBinario);
