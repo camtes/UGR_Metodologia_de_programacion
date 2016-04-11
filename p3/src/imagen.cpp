4a5c9341dd7976278e2d5743b7ac5739bf24c065
ccamposfuentes <carlos@ccamposfuentes.es>
2016-04-12 00:23:51 +0200

    Ejercicio 5

--- p3/src/imagen.cpp
+++ p3/src/imagen.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <string>
 #include <stdio.h>
+#include <cstring>
 #include "imagen.h"
 #include "pgm.h"
 #include "byte.h"
@@ -13,21 +14,18 @@ Imagen::Imagen(){
 };
 // Carlos
 Imagen::Imagen(int filas, int columnas) {
-  nfilas = filas;
-  ncolumnas = columnas;
-  for (int i=0; i<filas*columnas; i++) {
-    datos[i]=0;
-  }
+  crear(filas, columnas);
 }
 
 // Juanda
 void Imagen::crear(int filas, int columnas){
-
-	for (int i=0;i>=filas;i++){
-		for (int j=0;j>=columnas;j++){
-			set(i,j,0);
-		}
-	}
+  if(filas*columnas < MAXPIXELS){
+    nfilas = filas;
+    ncolumnas = columnas;
+    byte bzero = 0x00;
+    for(int i=0; i < filas*columnas; i++)
+      setPos(i,bzero);
+   }
 }
 
 // Carlos
@@ -42,14 +40,13 @@ int Imagen::columnas(){
 
 // Carlos
 void Imagen::set(int y, int x, byte v) {
-  datos[y*ncolumnas+x] = v;
+  if(y < nfilas && x < ncolumnas)
+    datos[ncolumnas*y + x] = v;
 }
 
 // Juanda
 byte Imagen::get(int y, int x){
-	int col;
-	col=columnas();
-	return (y*col)+x;
+	return datos[ncolumnas * y + x];
 }
 
 // Carlos
@@ -64,38 +61,53 @@ byte Imagen::getPos(int i){
 
 // Carlos
 bool Imagen::leerImagen(const char nombreFichero[]) {
-  bool result = false;
-
+  bool res = false;
   TipoImagen tipo = infoPGM(nombreFichero, nfilas, ncolumnas);
-  if (tipo != IMG_DESCONOCIDO) {
-    if (ncolumnas*nfilas < MAXPIXELS) {
-      return leerPGMBinario (nombreFichero, datos, nfilas, ncolumnas);
-    }
-  }
+
+  if (tipo == IMG_PGM_BINARIO && nfilas*ncolumnas < MAXPIXELS)
+    res = leerPGMBinario (nombreFichero, datos, nfilas, ncolumnas);
+
+  return res;
 }
 
 bool Imagen::escribirImagen(const char nombreFichero[], bool esBinario) {
-  bool result = false;
+  bool res = false;
 
-  if (esBinario) {
-    result = escribirPGMBinario(nombreFichero, datos, nfilas, ncolumnas);
-  }
+  if (esBinario)
+    res = escribirPGMBinario(nombreFichero, datos, nfilas, ncolumnas);
 
-  return result;
+  return res;
 }
 
 Imagen Imagen::plano (int k) {
-   Imagen nueva_imagen(nfilas, ncolumnas);
-   for(int i=0; i<ncolumnas; i++){
-      for(int j=0; j<nfilas; j++){
-         byte pixel = get(j,i);
-         bool is_one = getbit(pixel, k);
-         apagar(pixel);
-         if(is_one)
-            on(pixel,7);
-         nueva_imagen.set(j,i,pixel);
+  Imagen nueva_imagen(nfilas, ncolumnas);
+  for(int i=0; i<ncolumnas; i++) {
+    for(int j=0; j<nfilas; j++) {
+       byte pixel = get(j,i);
+       bool is_one = getbit(pixel, k);
+       apagar(pixel);
+
+       if(is_one)
+        on(pixel,7);
+       nueva_imagen.set(j,i,pixel);
+    }
+  }
+
+  return nueva_imagen;
+}
+
+bool Imagen::aArteASCII(const char grises[], char arteASCII[], int maxlong){
+   bool success = maxlong > (ncolumnas*nfilas + nfilas);
+   int pos = 0,
+      length = strlen(grises);
+   if(success){
+      for(int i=0; i<ncolumnas; i++){
+         for(int j=0; j<nfilas; j++){
+            arteASCII[pos++] = grises[1*get(i,j)*length/BITDEPTH];
+         }
+         arteASCII[pos++]='\n';
       }
    }
-
-   return nueva_imagen;
+   arteASCII[pos] = '\0';
+   return success;
 }
--- p3/src/testarteASCII.cpp
+++ p3/src/testarteASCII.cpp
@@ -8,7 +8,7 @@ int main(){
     char grises[] = "@#%xo;:,. ";
 
     char arteASCII[4501]; // 4500 + el \0
-       
+
     Imagen origen;
 
     // Leer la imagen gio.pgm
@@ -16,16 +16,16 @@ int main(){
 	   cerr << "error leyendo imagenes/gio.pgm\n";
 	   return 1;
     }
-	
+
     cout << "\nLa imagen en arte ASCII es:\n";
     if(origen.aArteASCII(grises, arteASCII, 4500))
         cout << arteASCII;
     else
-        cout << "La conversión no ha sido posible" << endl;  
-	
+        cout << "La conversiï¿½n no ha sido posible" << endl;
+
     cout << "Ahora Forzamos que no quepa. Debe aparecer un mensaje de error\n";
     if(origen.aArteASCII(grises, arteASCII, 4199))
         cout << arteASCII;
     else
-        cout << "La conversión no ha sido posible" << endl;
+        cout << "La conversiï¿½n no ha sido posible" << endl;
 }
